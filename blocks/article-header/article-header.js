9bffe26ef11c436b3e18c02f4812c914615c7602
Néstor de Dios <nestordedios@gmail.com>
2024-12-05 13:59:07 -0300

    article header

--- blocks/article-header/article-header.js
+++ blocks/article-header/article-header.js
@@ -6,5 +6,15 @@ import { loadFragment } from '../fragment/fragment.js';
  * @param {Element} block The block element
  */
 export default async function decorate(block) {
-  const author = getMetadata('Author');
+  const author = getMetadata('author');
+  block.querySelector('picture').parentElement.classList.add('background-image');
+
+  const articleInfo = document.createElement('div');
+  articleInfo.classList.add('article-info');
+  articleInfo.textContent = author;
+
+  const articleInfoWrapper = document.createElement('div');
+  articleInfoWrapper.classList.add('default-content-wrapper');
+  articleInfoWrapper.append(articleInfo);
+  block.append(articleInfoWrapper);
 }
