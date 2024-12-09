07a787f5547a05550df54b2b81066c50b45ebc0f
Néstor de Dios <nestordedios@gmail.com>
2024-12-09 10:05:34 -0300

    placeholders

--- scripts/aem.js
+++ scripts/aem.js
@@ -494,41 +494,9 @@ function decorateSections(main) {
 
 /**
  * Gets placeholders object.
- * @param {string} [prefix] Location of placeholders
  * @returns {object} Window placeholders object
  */
 // eslint-disable-next-line import/prefer-default-export
-/*async function fetchPlaceholders(prefix = 'default') {
-  window.placeholders = window.placeholders || {};
-  if (!window.placeholders[prefix]) {
-    window.placeholders[prefix] = new Promise((resolve) => {
-      fetch(`${prefix === 'default' ? '' : prefix}/placeholders.json`)
-        .then((resp) => {
-          if (resp.ok) {
-            return resp.json();
-          }
-          return {};
-        })
-        .then((json) => {
-          const placeholders = {};
-          json.data
-            .filter((placeholder) => placeholder.Key)
-            .forEach((placeholder) => {
-              placeholders[toCamelCase(placeholder.Key)] = placeholder.Text;
-            });
-          window.placeholders[prefix] = placeholders;
-          resolve(window.placeholders[prefix]);
-        })
-        .catch(() => {
-          // error loading placeholders
-          window.placeholders[prefix] = {};
-          resolve(window.placeholders[prefix]);
-        });
-    });
-  }
-  return window.placeholders[`${prefix}`];
-}*/
-
 export async function fetchPlaceholders() {
   if (!window.placeholders) {
     const resp = await fetch(`${getRootPath()}/placeholders.json`);
