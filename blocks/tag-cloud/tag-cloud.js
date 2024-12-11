151b4419068078fa113d0199b99365a1d5c0a6b8
Néstor de Dios <nestordedios@gmail.com>
2024-12-11 11:50:47 -0300

    tag cloud

---
+++ blocks/tag-cloud/tag-cloud.js
@@ -0,0 +1,26 @@
+import { getMetadata, fetchPlaceholders, fetchTags } from '../../scripts/aem.js';
+
+function buildTagList(placeholders, tags){
+  const taglist = getMetadata('article:tag');
+  if (!taglist) return;
+  const container = document.createElement('div');
+  taglist.split(',').forEach((tag) => {
+    const tagName = tags[tag] ? tags[tag] : tag;
+    const button = document.createElement('button');
+    button.classList.add('btn-tag-filter');
+    button.textContent = tagName;
+    container.append(button);
+  });
+  return container;
+}
+
+/**
+ * loads and decorates the tag cloud
+ * @param {Element} block The block element
+ */
+export default async function decorate(block) {
+  buildBase(block);
+  const placeholders = await fetchPlaceholders();
+  const tags = await fetchTags();
+  block.append(buildTagList(placeholders, tags));
+}
