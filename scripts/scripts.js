34d813cfbefd5e21cdc1af66d5e1ec3c85ad656b
Néstor de Dios <nestordedios@gmail.com>
2024-12-12 16:49:13 -0300

    plugin

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -321,3 +321,21 @@ async function loadPage() {
 }
 
 loadPage();
+
+const doFoo = ({ detail: payload }) => {
+  console.log('a custom event happened', payload);
+  // your custom code goes here
+};
+
+const sk = document.querySelector('aem-sidekick');
+if (sk) {
+  // sidekick already loaded
+    sk.addEventListener('custom:foo', doFoo);
+} else {
+  // wait for sidekick to be loaded
+  document.addEventListener('sidekick-ready', () => {
+    // sidekick now loaded
+    document.querySelector('aem-sidekick')
+      .addEventListener('custom:foo', doFoo);
+  }, { once: true });
+}
